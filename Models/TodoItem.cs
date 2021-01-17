7bdd04c37e721b03ddc3ed69260648be73434dea
Ryo Watanabe <ryo33345@gmail.com>
2021-01-17 21:18:02 +0900

    モデル作成

---
+++ Models/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace asp_net_web_api_sample.Models {
+  public class TodoItem {
+    public long Id {get; set;}
+    public string Name {get; set;}
+
+    public bool IsComplete {get; set;}
+  }
+}
